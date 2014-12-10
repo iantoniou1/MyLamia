21fd9797e81cced4806ae1c1e4f56edb00c6ab24
Antoniou Ioanna <antoniou2010@windowslive.com>
2014-12-10 11:40:15 +0200

    commit

---
+++ src/com/example/android/hcgallery/Directory.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.hcgallery;
+
+public class Directory {
+    private static DirectoryCategory[] mCategories;
+
+    public static void initializeDirectory() {
+      mCategories = new DirectoryCategory[] {
+                new DirectoryCategory("ΠΛΟΗΓΗΣΗ", new DirectoryEntry[] {
+                		
+                		new DirectoryEntry("Καλώς Ήρθατε", R.string.Welcome),
+                        new DirectoryEntry("Αξιοθέατα", R.string.Axiotheata),
+                		// new DirectoryEntry("Αξιοθέατα", new Subcategory[]{
+                         //		new Subcategory("ioanna", R.string.Axiotheata)}),
+                        new DirectoryEntry("Εστιατόρια & Καφετέριες", R.string.Kafes),
+                        new DirectoryEntry("Νυχτερινή Ζωή", R.string.Nightlife),
+                        new DirectoryEntry("Ώρα για ψώνια", R.string.Shopping),
+                        new DirectoryEntry("Υπηρεσίες Έκτακτης Ανάγκης", R.string.Emergency),
+                                              
+                		 }),
+                new DirectoryCategory("ΧΑΡΤΕΣ", new DirectoryEntry[] {}),
+                       
+                new DirectoryCategory("ΚΟΝΤΙΝΑ ΣΗΜΕΙΑ", new DirectoryEntry[] {
+                       
+                         }),
+                };
+
+    }
+    
+
+    public static int getCategoryCount() {
+        return mCategories.length;
+    }
+
+    public static DirectoryCategory getCategory(int i) {
+        return mCategories[i];
+    }
+}
---
+++ src/com/example/android/hcgallery/NightlifeActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.hcgallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class NightlifeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_nightlife);
+    }
+}
